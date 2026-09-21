1af5159ce5e746a6f578d9984330167b89788a2f
Bogdan <bogdan@automaintenance.dev>
2026-09-21 18:53:20 +0000

    Final ro-locale TypeScript fix

--- lib/ro-locale.ts
+++ lib/ro-locale.ts
@@ -5,32 +5,32 @@ export const roLocale: Locale = {
   formatDistance: () => '',
   formatRelative: () => '',
   localize: {
-    ordinalNumber: (num) => `${num}`,
+    ordinalNumber: (num: any) => `${num}`,
     era: () => '',
     quarter: () => '',
-    month: (num) => {
+    month: (num: number) => {
       const months = [
         'Ianuarie', 'Februarie', 'Martie', 'Aprilie', 'Mai', 'Iunie',
         'Iulie', 'August', 'Septembrie', 'Octombrie', 'Noiembrie', 'Decembrie'
       ]
       return months[num]
     },
-    day: (num) => {
+    day: (num: number) => {
       const days = ['Duminică', 'Luni', 'Marți', 'Miercuri', 'Joi', 'Vineri', 'Sâmbătă']
       return days[num]
     },
     dayPeriod: () => '',
   },
   match: {
-    ordinalNumberPattern: /^\d+/i as any,
-    era: /^(î|d)/i as any,
-    quarter: /^[1234]/i as any,
-    month: /^(0[1-9]|1[0-2])/i as any,
-    day: /^(0[1-9]|[12]\d|3[01])/i as any,
-    dayPeriod: /^(a\.m\.|p\.m\.)/i as any,
+    ordinalNumberPattern: (/^\d+/i) as any,
+    era: (/^(î|d)/i) as any,
+    quarter: (/^[1234]/i) as any,
+    month: (/^(0[1-9]|1[0-2])/i) as any,
+    day: (/^(0[1-9]|[12]\d|3[01])/i) as any,
+    dayPeriod: (/^(a\.m\.|p\.m\.)/i) as any,
   },
   options: {
-    weekStartsOn: 1,
-    firstWeekContainsDate: 4,
+    weekStartsOn: 1 as any,
+    firstWeekContainsDate: 4 as any,
   },
 }
